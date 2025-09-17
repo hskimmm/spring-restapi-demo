f84e5116a863a99d3830d9581d70981a40434a5e
Kim Hyeong Seop <fbhydfa@gmail.com>
2025-09-17 11:16:17 +0900

    feat: init

---
+++ spring-reatapi/src/main/java/com/spring/restapi/SpringReatapiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReatapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReatapiApplication.class, args);
+    }
+
+}
